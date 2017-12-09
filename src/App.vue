fb3d7588834644ba1253a0177c53293cff0b1d0b
taishikato <taishi.k0903@gmail.com>
2017-12-09 12:18:54 +0900

    final

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div class="">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/firstroute/takato">FirstRoute</router-link></li>
+        <li><router-link to="/firstroute/takato/child">FirstRouteChild</router-link></li>
+      </ul>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -19,5 +26,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import FirstRoute from '@/components/FirstRoute'
+import FirstRouteChild from '@/components/FirstRouteChild'
 
 Vue.use(Router)
 
@@ -10,6 +12,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/firstroute/:name',
+      name: 'FirstRoute',
+      component: FirstRoute,
+      children: [
+        {
+          path: 'child',
+          component: FirstRouteChild
+        }
+      ]
     }
   ]
 })
